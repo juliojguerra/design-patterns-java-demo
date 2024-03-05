ff50aba2cd10745111f2a0c00d663fbb53098efd
Julio Guerra <j.guerratamayo@gmail.com>
2024-03-04 22:46:51 -0500

    Update tests and TravelHomePAge PO

--- src/main/java/org/jwar/PageObjects/TravelHomePage.java
+++ src/main/java/org/jwar/PageObjects/TravelHomePage.java
@@ -1,14 +1,20 @@
 package org.jwar.PageObjects;
 
+import org.jwar.AbstractComponents.SearchFlightAvail;
+import org.jwar.AbstractComponents.StrategyFactor;
 import org.jwar.PageComponents.FooterNav;
+import org.jwar.PageComponents.MultiTrip;
 import org.jwar.PageComponents.NavigationBar;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.util.HashMap;
 
 public class TravelHomePage {
     By sectionElement = By.id("traveller-home");
     By footerNavSectionElement = By.id("buttons");
     WebDriver driver;
+    SearchFlightAvail searchFlightAvail;
     public TravelHomePage(WebDriver driver){
         this.driver = driver;
     }
@@ -22,5 +28,12 @@ public class TravelHomePage {
 
     public FooterNav GetFooterNav() {
         return new FooterNav(driver, sectionElement);
+    }
+    public void SetBookingStrategy(String strategyType){
+        StrategyFactor strategyFactor = new StrategyFactor(driver);
+        this.searchFlightAvail = strategyFactor.CreateStrategy(strategyType);
+    }
+    public void CheckAvail(HashMap<String, String> reservationDetails){
+        this.searchFlightAvail.CheckAvail(reservationDetails);
     }
 }
